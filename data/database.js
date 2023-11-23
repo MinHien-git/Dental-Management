d5e30e36ba878c8577e918bca0803f2505212f62
maiminhnhat <64275316+MinHien-git@users.noreply.github.com>
2023-11-23 14:05:01 +0700

    add

--- data/database.js
+++ data/database.js
@@ -1,29 +1,18 @@
-const sql = require("mssql");
-const sqlConfig = {
-  user: process.env.DB_USER | "BRIGHT/hienb",
-  password: process.env.DB_PWD,
-  database: process.env.DB_NAME | "QLDT_1",
+const sql = require("mssql/msnodesqlv8");
+
+var config = {
   server: "localhost",
-  pool: {
-    max: 10,
-    min: 0,
-    idleTimeoutMillis: 30000,
-  },
-  options: {
-    encrypt: true, // for azure
-    trustServerCertificate: false, // change to true for local dev / self-signed certs
-  },
+  user: "sa",
+  password: "123456",
+  database: "QLDT_1",
+  driver: "msnodesqlv8",
 };
 
-const connect_to_database = async () => {
-  try {
-    // make sure that any items are correctly URL encoded in the connection string
-    await sql.connect(sqlConfig);
-    const result = await sql.query`select * from giaovien where id = ${1}`;
-    console.dir(result);
-  } catch (err) {
-    // ... error checks
-  }
-};
+const conn = new sql.ConnectionPool(config).connect().then((pool) => {
+  return pool;
+});
 
-module.exports = { connect_to_database };
+module.exports = {
+  conn: conn,
+  sql: sql,
+};
--- index.js
+++ index.js
@@ -1,31 +1,15 @@
 const express = require("express");
-const db = require("./data/database");
 const app = express();
 const PORT = 5000 | process.env.PORT;
-const mssql = require("mssql");
+const db = require("./data/database");
+const { pool } = require("mssql");
 
-app.get("/", function (req, res) {
-  // Config your database credential
-  const config = {
-    user: `SA`,
-    server: "localhost",
-    database: "QLDT_1",
-  };
-
-  // Connect to your database
-  mssql.connect(config, function (err) {
-    // Create Request object to perform
-    // query operation
-    let request = new mssql.Request();
-
-    // Query to the database and get the records
-    request.query("select * from GIAOVIEN", function (err, records) {
-      if (err) console.log(err);
-
-      // Send records as a response
-      // to browser
-      res.send(records);
-    });
+app.get("/", async (req, res) => {
+  let gv = await db.conn;
+  let query = "SELECT * FROM GIAOVIEN";
+  return await pool.request().query(query, function (err, data) {
+    console.log("err ", data);
+    res.send({ result: data.recordset });
   });
 });
 app.listen(PORT, () => {
