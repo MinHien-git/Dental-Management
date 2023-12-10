160e728d539a9d580eed6982127cf2f5d1fb544c
MinHien <64275316+MinHien-git@users.noreply.github.com>
2023-12-10 14:29:01 +0700

    add

---
+++ models/users/employee.models.js
@@ -0,0 +1,11 @@
+class Employee {
+  constructor(MaND, TenND, NgaySinhND, GioiTinhND, MatKhau, PhongKham) {
+    this.MaND = MaND;
+    this.TenND = TenND;
+    this.NgaySinhND = NgaySinhND;
+    this.GioiTinhND = GioiTinhND;
+    this.MatKhau = MatKhau;
+    this.LoaiNguoiDung = "Em";
+    this.PhongKham = PhongKham;
+  }
+}
