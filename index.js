bcade6e2d645223d62f33244f9804d07e07fac34
maiminhnhat <64275316+MinHien-git@users.noreply.github.com>
2023-12-11 21:10:56 +0700

    update

--- index.js
+++ index.js
@@ -19,11 +19,64 @@ const config = {
 app.get("/", async (req, res) => {
   try {
     await sql.connect(config);
+    const request = new sql.Request();
+    request.input();
+    const result = await request.execute();
     res.send("DB Connected");
   } catch (err) {
     res.send(err);
   }
 });
+
+app.get("/login", async (req, res) => {
+  try {
+    await sql.connect(config);
+    const request = new sql.Request();
+    request.input("@ten", "Luong Minh Hien");
+    request.input("@mk", "NK123456");
+    request.output("@kq", sql.NVarChar);
+    const result = await request.execute("PROC_DangNhap", (err, result) => {
+      console.log(result.output);
+    });
+    res.send("DB Connected");
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    await sql.connect(config);
+    const request = new sql.Request();
+    request.input("@ten", "Luong Minh Hien");
+    request.input("@mk", "NK123456");
+    request.output("@kq", sql.NVarChar);
+    const result = await request.execute("PROC_DangNhap", (err, result) => {
+      console.log(result.output);
+    });
+    res.send("DB Connected");
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    await sql.connect(config);
+    const request = new sql.Request();
+    request.input("@ten", "Luong Minh Hien");
+    request.input("@mk", "NK123456");
+    request.output("@kq", sql.NVarChar);
+    const result = await request.execute("PROC_DangNhap", (err, result) => {
+      console.log(result.output);
+    });
+    res.send("DB Connected");
+  } catch (err) {
+    res.send(err);
+  }
+})
+
+app.post('/')
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
